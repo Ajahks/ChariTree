7ece82dcd438c2677a77355a0fd7f5c9eddac7f7
Dylan Greicius <dgreiciu@ucsd.edu>
2018-10-13 16:48:34 -0700

    Disabled back button on main page to prevent errors with account creation

--- src/MainPage.js
+++ src/MainPage.js
@@ -7,7 +7,13 @@ export default class MainPageScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = { text: '' };
-  } 
+  };
+
+  /* vvv This disables the back button vvv */
+  static navigationOptions = {
+    headerLeft: null
+  };
+
   render() {
     user = this.props.navigation.getParam('user', 'Error')
 
